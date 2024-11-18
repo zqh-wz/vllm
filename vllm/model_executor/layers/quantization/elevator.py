fa3afe14869fc086558b7df8d925fa087b231766
zqh23 <zqh@fuse0.fuse0.lab.pacman-thu.org>
2024-11-18 22:29:17 +0800

    integrate elevator

---
+++ vllm/model_executor/layers/quantization/elevator.py
@@ -0,0 +1,176 @@
+from typing import Any, Dict, List, Optional
+
+import torch
+
+from vllm.logger import init_logger
+from vllm.model_executor.layers.linear import LinearBase, LinearMethodBase
+from vllm.model_executor.layers.quantization.base_config import QuantizationConfig
+from vllm.model_executor.parameter import PackedvLLMParameter, RowvLLMParameter, GroupQuantScaleParameter
+
+import elevator
+
+logger = init_logger(__name__)
+
+class ElevatorConfig(QuantizationConfig):
+    """Config class for Elevator"""
+
+    def __init__(
+        self,
+        weight_bits: int,
+        group_size: int,
+    ):
+        self.weight_bits = weight_bits
+        self.pack_factor = 32 // weight_bits
+        self.group_size = group_size
+
+    @classmethod
+    def from_config(cls, config: Dict[str, Any]) -> "ElevatorConfig":
+        weight_bits = cls.get_from_keys(config, ["bits"])
+        group_size = cls.get_from_keys(config, ["group_size"])
+        return cls(weight_bits, group_size)
+
+    @classmethod
+    def override_quantization_method(cls, hf_quant_cfg, user_quant) -> Optional[str]:
+        if user_quant == "elevator":
+            logger.warning("Using elevator kernel.")
+            return user_quant
+        return None
+    
+    @classmethod
+    def get_min_capability(cls) -> int:
+        return 70
+    
+    def get_supported_act_dtypes(self) -> List[torch.dtype]:
+        return [torch.float16]
+    
+    def get_quant_method(self, layer: torch.nn.Module,
+                         prefix: str) -> Optional["ElevatorLinearMethod"]:
+        if isinstance(layer, LinearBase):
+            return ElevatorLinearMethod(self)
+        return None
+    
+    def get_name(self) -> str:
+        return "elevator"
+
+    @staticmethod
+    def get_config_filenames() -> List[str]:
+        """List of filenames to search for in the model directory."""
+        raise NotImplementedError
+    
+
+class ElevatorLinearMethod(LinearMethodBase):
+    """Linear method for Elevator.
+
+    Args:
+        quant_config: The Elevator quantization config.
+    """
+
+    def __init__(self, quant_config: ElevatorConfig):
+        self.quant_config = quant_config
+    
+    def create_weights(
+        self,
+        layer: torch.nn.Module,
+        input_size_per_partition: int,
+        output_partition_sizes: List[int],
+        input_size: int,
+        output_size: int,
+        params_dtype: torch.dtype,
+        **extra_weight_attrs
+    ):
+        output_size_per_partition = sum(output_partition_sizes)
+        weight_loader = extra_weight_attrs.get("weight_loader")
+        
+        qweight = PackedvLLMParameter(
+            data=torch.empty(
+                input_size_per_partition // self.quant_config.pack_factor,
+                output_size_per_partition,
+                dtype=torch.int32,
+            ),
+            input_dim=0,
+            output_dim=1,
+            packed_dim=0,
+            packed_factor=self.quant_config.pack_factor,
+            weight_loader=weight_loader
+        )
+
+        g_idx = RowvLLMParameter(
+            data=torch.empty(
+                input_size_per_partition,
+                dtype=torch.int32,
+            ),
+            input_dim=0,
+            weight_loader=weight_loader
+        )
+
+        scales_and_zp_size = input_size_per_partition // self.quant_config.group_size
+
+        scales = GroupQuantScaleParameter(
+            data=torch.empty(
+                scales_and_zp_size,
+                output_size_per_partition,
+                dtype=params_dtype,
+            ),
+            input_dim=0,
+            output_dim=1,
+            weight_loader=weight_loader
+        )
+        qzeros = PackedvLLMParameter(
+            data=torch.empty(
+                scales_and_zp_size,
+                output_size_per_partition // self.quant_config.pack_factor,
+                dtype=torch.int32,
+            ),
+            input_dim=0,
+            output_dim=1,
+            packed_dim=1,
+            packed_factor=self.quant_config.pack_factor,
+            weight_loader=weight_loader
+        )
+
+        layer.register_parameter("qweight", qweight)
+        layer.register_parameter("g_idx", g_idx)
+        layer.register_parameter("scales", scales)
+        layer.register_parameter("qzeros", qzeros)
+    
+    def process_weights_after_loading(self, layer: torch.nn.Module):
+        input_size = layer.qweight.shape[0] * self.quant_config.pack_factor
+        output_size = layer.qweight.shape[1]
+        device = layer.qweight.device
+
+        row_idx = torch.arange(input_size, device=device, dtype=torch.int32) // self.quant_config.pack_factor
+        row_offset = torch.arange(input_size, device=device, dtype=torch.int32) % self.quant_config.pack_factor
+        mask = (1 << self.quant_config.weight_bits) - 1
+        qweight_uncompressed = (layer.qweight[row_idx] >> (row_offset * self.quant_config.weight_bits).unsqueeze(1)) & mask
+
+        col_idx = torch.arange(output_size, device=device, dtype=torch.int32) // self.quant_config.pack_factor
+        col_offset = torch.arange(output_size, device=device, dtype=torch.int32) % self.quant_config.pack_factor
+        qzeros_uncompressed = (layer.qzeros[:, col_idx] >> (col_offset * self.quant_config.weight_bits).unsqueeze(0)) & mask
+
+        kernel_name = "gptq_gemv"
+        template_args = {
+            "K": input_size,
+            "N": output_size,
+            "GS": self.quant_config.group_size,
+            "BLOCK_N": 1,
+            "BLOCK_SIZE": 32,
+        }
+
+        layer.elevator_linear = elevator.auto_get_layer(
+            kernel_name=kernel_name,
+            template_args=template_args,
+            init_params=(
+                qweight_uncompressed,
+                layer.scales,
+                qzeros_uncompressed,
+            ),
+            device=device,
+        )
+
+    def apply(self,
+              layer: torch.nn.Module,
+              x: torch.Tensor,
+              bias: Optional[torch.Tensor] = None) -> torch.Tensor:
+        """Apply the weights in layer to the input tensor.
+        Expects create_weights to have been called before on the layer."""
+        return layer.elevator_linear(x)
